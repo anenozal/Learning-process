438dafc5b812a28bf67f69a640842495cb446c0c
anenozal <anenotizalschk@gmail.com>
2019-05-25 16:55:28 +0300

    BoJack-mobile-media

--- bojack/src/index.js
+++ bojack/src/index.js
@@ -1,4 +1,11 @@
+import './sass/mobile.sass';
 import './scss/main.scss';
 import './sass/main.sass';
+
+
+document.body.style.webkitTransform =  'scale(1)';
+document.body.style.msTransform =   'scale(100)';
+document.body.style.transform = 'scale(1)';
+document.body.style.zoom = screen.logicalXDPI / screen.deviceXDPI;
 
 console.log('Hello, world!');
